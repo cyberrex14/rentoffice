b196226ba870e76c31f03b8d3b561c9dcc167870
cyberrex14 <qcyberrex@gmail.com>
2026-04-23 12:10:01 +0700

    Feat : menambahkan table BookingTranscation

---
+++ app/Http/Controllers/BookingTransactionsController.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\BookingTransactions;
+use Illuminate\Http\Request;
+
+class BookingTransactionsController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(BookingTransactions $bookingTransactions)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(BookingTransactions $bookingTransactions)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, BookingTransactions $bookingTransactions)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(BookingTransactions $bookingTransactions)
+    {
+        //
+    }
+}
---
+++ database/migrations/2026_04_23_050147_create_booking_transactions_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('booking_transactions', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->string('phone_number');
+            $table->string('booking_trx');
+            $table->text('office_space_id');
+            $table->integer('total_amount');
+            $table->integer('duration');
+            $table->date('started_at');
+            $table->date('ended_at');
+            $table->boolean('is_paid');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('booking_transactions');
+    }
+};
