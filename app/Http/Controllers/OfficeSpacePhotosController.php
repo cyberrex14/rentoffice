341022bd5471a4bffd82ee4baa017b5213b59f99
cyberrex14 <qcyberrex@gmail.com>
2026-04-23 11:54:42 +0700

    Feat : Menambahkan Table OfficeSpacePhotos

---
+++ app/Http/Controllers/OfficeSpacePhotosController.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\OfficeSpacePhotos;
+use Illuminate\Http\Request;
+
+class OfficeSpacePhotosController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(OfficeSpacePhotos $officeSpacePhotos)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(OfficeSpacePhotos $officeSpacePhotos)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, OfficeSpacePhotos $officeSpacePhotos)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(OfficeSpacePhotos $officeSpacePhotos)
+    {
+        //
+    }
+}
